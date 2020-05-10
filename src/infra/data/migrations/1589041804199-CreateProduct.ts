ee64741abdc7735496e7e44e6fdc0b882e74cdae
Robson Maia <robsonmvieira@gmail.com>
2020-05-10 08:45:39 -0300

    feat: create product create migration

---
+++ src/infra/data/migrations/1589041804199-CreateProduct.ts
@@ -0,0 +1,62 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateProduct1589041804199 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'products',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+              generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()'
+
+            },
+            {
+              name: 'title',
+              type: 'varchar',
+              isUnique: true,
+            },
+            {
+              name: 'price',
+              type: 'varchar',
+            },
+            {
+              name: 'available',
+              type: 'boolean',
+              default: true,
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp with time zone',
+              isNullable: false,
+              default: 'now()'
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp with time zone',
+              isNullable: false,
+              default: 'now()'
+            }
+          ]
+        })
+      )
+      await queryRunner.createForeignKey('products',
+        new TableForeignKey({
+          name: 'productCompany',
+          columnNames: ['id'],
+          referencedColumnNames: ['id'],
+          referencedTableName: 'companies',
+          onDelete: 'CASCADE'
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropForeignKey('products', 'productCompany')
+      await queryRunner.dropTable('products')
+    }
+
+}
