e7b37681de6c8c885fec79f2e306d98dd1d90775
Robson Maia <robsonmvieira@gmail.com>
2020-05-10 08:42:59 -0300

    feat: implement CreateCompanyService

---
+++ src/domain/services/company/CreateCompanyService.ts
@@ -0,0 +1,17 @@
+import Company from "@domain/contexts/users/entities/Company";
+import { injectable, inject } from "tsyringe";
+import CompanyRepository from "@infra/repositories/CompanyRepository";
+import ICreateCompanyService from "@domain/contexts/users/contracts/company/services/ICreateCompanyService";
+
+@injectable()
+export default class CreateCompanyService implements ICreateCompanyService {
+  constructor(
+    @inject('CompanyRepository') private companyRepository: CompanyRepository
+
+  ) {
+  }
+  async create(company: Company): Promise<Company> {
+    return this.companyRepository.create(company)
+  }
+
+}
