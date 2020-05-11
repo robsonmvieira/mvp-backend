ba2df1041055046435f2e17920ab89b59cabb1ed
Robson Maia <robsonmvieira@gmail.com>
2020-05-10 22:58:24 -0300

    feat: update app entrypoint

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -3,6 +3,8 @@ import express from "express";
 import "./infra/data"
 import "@infra/container"
 import companyRoutes from '@domain/contexts/users/routes/companyRoutes'
+import addressRoutes from '@domain/contexts/users/routes/addressRoutes'
+import userRoutes from '@domain/contexts/users/routes/userRoutes'
 
 const app = express();
 
@@ -10,6 +12,8 @@ const app = express();
 app.use(express.json())
 
 app.use('/companies', companyRoutes)
+app.use('/address', addressRoutes)
+app.use('/users', userRoutes)
 
 
 
