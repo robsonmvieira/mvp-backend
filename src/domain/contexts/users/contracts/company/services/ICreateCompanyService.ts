46ca8825142fb765c56f00d2ba9db9ee0c86ae35
Robson Maia <robsonmvieira@gmail.com>
2020-05-10 23:04:54 -0300

    feat: create contract to  get users service

--- src/domain/contexts/users/contracts/company/services/ICreateCompanyService.ts
+++
@@ -1,5 +0,0 @@
-import Company from "@domain/contexts/users/entities/Company";
-
-export default interface ICreateCompanyService {
-  create(company: Company):Promise<Company>
-}
--- src/domain/contexts/users/contracts/company/services/IGetCompaniesService.ts
+++
@@ -1,5 +0,0 @@
-import Company from "@domain/contexts/users/entities/Company";
-
-export default interface IGetCompaniesService {
-  getCompanies():Promise<Company[]>
-}
