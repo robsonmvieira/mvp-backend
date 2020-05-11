f1240b03749fe49ccd37549284c050f151e4e063
Robson Maia <robsonmvieira@gmail.com>
2020-05-10 23:11:53 -0300

    feat: update container DI

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -1,6 +1,12 @@
 import {container} from 'tsyringe'
 import ICompanyRepository from '@domain/contracts/ICompanyRepository'
 import CompanyRepository from '@infra/repositories/CompanyRepository'
+import IAddressRepository from '@domain/contracts/IAddressRepository'
+import AddressRepository from '@infra/repositories/AddressRepository'
+import IUserRepository from '@domain/contracts/IUserRepository'
+import UserRepository from '@infra/repositories/UserRepository'
 
 
 container.registerSingleton<ICompanyRepository>("CompanyRepository", CompanyRepository)
+container.registerSingleton<IAddressRepository>("AddressRepository", AddressRepository)
+container.registerSingleton<IUserRepository>("UserRepository", UserRepository)
