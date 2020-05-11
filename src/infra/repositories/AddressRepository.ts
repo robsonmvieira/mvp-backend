e3f8ff2e0bb11f2dfc0fb8dbe931d7843fe25788
Robson Maia <robsonmvieira@gmail.com>
2020-05-10 23:12:46 -0300

    feat: implement Address Repository

--- src/infra/repositories/AddressRepository.ts
+++ src/infra/repositories/AddressRepository.ts
@@ -1,4 +1,31 @@
-import { Repository } from "typeorm";
+import { Repository, getRepository } from "typeorm";
 import Address from "domain/contexts/users/entities/Address";
+import IAddressRepository from "@domain/contracts/IAddressRepository";
 
-export default class AddressRepository extends Repository<Address> {}
+export default class AddressRepository implements IAddressRepository {
+  private readonly orm: Repository<Address>
+
+  constructor() {
+    this.orm = getRepository(Address)
+  }
+
+  async create(address: Address): Promise<Address> {
+    const result = await this.orm.save(address)
+    return result
+  }
+
+  async index(): Promise<Address[]> {
+   return this.orm.find()
+  }
+
+  show(id: string): Promise<Address | undefined> {
+    throw new Error("Method not implemented.");
+  }
+  update(id: string, address: Address): Promise<Address | undefined> {
+    throw new Error("Method not implemented.");
+  }
+  remove(id: string): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+
+}
