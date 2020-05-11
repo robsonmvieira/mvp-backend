0247942057c21a8d69bf1dcb583bf73ec368310b
Robson Maia <robsonmvieira@gmail.com>
2020-05-10 23:10:10 -0300

    feat: implement get users service

---
+++ src/domain/services/user/GetUsersServices.ts
@@ -0,0 +1,14 @@
+import IGetUsersServices from "@domain/contexts/users/contracts/user/IGetUsersServices";
+import User from "@domain/contexts/users/entities/User";
+import { injectable, inject } from "tsyringe";
+import UserRepository from "@infra/repositories/UserRepository";
+
+@injectable()
+export default class GetUsersServices implements IGetUsersServices {
+  constructor(@inject('UserRepository') private userRepo: UserRepository){}
+
+  async index(): Promise<User[]> {
+    return await this.userRepo.index()
+  }
+
+}
