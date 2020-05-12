0fc945e6ba60a246b631787efa01ca215d687eb4
Robson Maia <robsonmvieira@gmail.com>
2020-05-11 23:09:32 -0300

    feat: implement create user service

---
+++ src/domain/services/user/CreateUserService.ts
@@ -0,0 +1,18 @@
+import ICreateUserService from "@domain/contexts/users/contracts/user/ICreateUserService";
+import { injectable, inject } from "tsyringe";
+import User from "@domain/contexts/users/entities/User";
+import UserRepository from "@infra/repositories/UserRepository";
+import HasherAdapter from "@infra/adapters/HashAdapter/implementation/HasherAdapter";
+
+@injectable()
+export default class CreateUserService implements ICreateUserService {
+
+  constructor(@inject('UserRepository') private userRep: UserRepository) {}
+  async create(user: User): Promise<User> {
+   const hasherClass = new HasherAdapter()
+   user.password = await hasherClass.encrypt(user.password)
+   const result = await this.userRep.create(user)
+   delete result.password
+   return result
+  }
+}
