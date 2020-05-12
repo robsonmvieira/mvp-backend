8bebd28bcd636f544eecf7d580d89d6a1f3bea82
Robson Maia <robsonmvieira@gmail.com>
2020-05-12 13:55:08 -0300

    feat: update to save new user and him address

--- src/application/userController.ts
+++ src/application/userController.ts
@@ -2,12 +2,32 @@ import {Request, Response} from 'express'
 import { container } from 'tsyringe'
 import GetUsersServices from '@domain/services/user/GetUsersServices'
 import CreateUserService from '@domain/services/user/CreateUserService'
+import CreateAddressService from '@domain/services/address/CreateAddressService'
 
 export default class UserController {
 
   public async create(req:Request,res: Response ):Promise<Response> {
-    const service = container.resolve(CreateUserService)
-    const result = await service.create(req.body)
+    const userService = container.resolve(CreateUserService)
+    const addService = container.resolve(CreateAddressService)
+
+    const {country, state, city, street, number, zip_code } = req.body
+
+    const newAddress = { country, state, city, street, number, zip_code }
+
+    const {name, cpf, email, phone_number, password } = req.body
+
+    const createdAddress = await addService.create(newAddress)
+
+    const newUser = {
+      name,
+      cpf,
+      email,
+      phone_number,
+      password,
+      address_id: createdAddress.id
+    }
+
+    const result = await userService.create(newUser)
     delete result.password
     return res.status(201).json(result)
   }
