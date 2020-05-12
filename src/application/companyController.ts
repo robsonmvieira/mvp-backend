c98a65581f0ab4147761023ce87ff624b6cc97f2
Robson Maia <robsonmvieira@gmail.com>
2020-05-12 13:53:53 -0300

    feat: update  to save new company and him address

--- src/application/companyController.ts
+++ src/application/companyController.ts
@@ -2,12 +2,32 @@ import {Request, Response} from 'express'
 import { container } from 'tsyringe'
 import CreateCompanyService from '@domain/services/company/CreateCompanyService'
 import GetCompaniesService from '@domain/services/company/GetCompaniesService'
+import CreateAddressService from '@domain/services/address/CreateAddressService'
 
 export default class CompanyController {
 
   public async create(req:Request,res: Response ):Promise<Response> {
-    const service = container.resolve(CreateCompanyService)
-    const result = await service.create(req.body)
+    const companyService = container.resolve(CreateCompanyService)
+    const addService = container.resolve(CreateAddressService)
+
+    const {country, state, city, street, number, zip_code } = req.body
+    const address = {
+      country,
+      state,
+      city,
+      street,
+      number,
+      zip_code
+    }
+    const createdAddress = await addService.create(address)
+    const { cnpj, name, email } = req.body
+    const createCompany = {
+      cnpj,
+      name,
+      email,
+      address_id: createdAddress.id
+    }
+    const result = await companyService.create(createCompany)
     return res.status(201).json(result)
   }
 
