8bba57fd349f8e687057161f08275a0f1837006b
Robson Maia <robsonmvieira@gmail.com>
2020-05-12 15:06:07 -0300

    update: add interface to post payload

--- src/infra/repositories/UserRepository.ts
+++ src/infra/repositories/UserRepository.ts
@@ -2,19 +2,32 @@ import { Repository, getRepository } from "typeorm";
 import User from "domain/contexts/users/entities/User";
 import IUserRepository from "@domain/contracts/IUserRepository";
 
+interface userPost {
+  name: string;
+  cpf: string ;
+  email: string;
+  phone_number: string;
+  password: string;
+  address_id: string;
+ }
+
 export default class UserRepository implements IUserRepository {
   private readonly orm: Repository<User>
+
   constructor() {
     this.orm = getRepository(User)
   }
-  async create(user: User): Promise<User> {
+
+  async create(user: userPost): Promise<User> {
     const result = await this.orm.save(user)
     return result
   }
+
   async index(): Promise<User[]> {
     const users = await this.orm.find()
     return users
   }
+
   async show(id: string): Promise<User | undefined> {
    const userExists = await this.orm.findOne(id)
    if(userExists) {
@@ -22,6 +35,7 @@ export default class UserRepository implements IUserRepository {
    }
    return undefined
   }
+
   async update(id: string, user: User): Promise<User | undefined> {
     const userExists = await this.orm.findOne(id)
     if(userExists) {
@@ -30,6 +44,7 @@ export default class UserRepository implements IUserRepository {
     }
     return undefined
   }
+
   async remove(id: string): Promise<boolean> {
     const userExists = await this.orm.findOne(id)
     if(userExists) {
