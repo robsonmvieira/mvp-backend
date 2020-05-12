b63cff7084ba9722785d6e1b5741a39b89b5c4d1
Robson Maia <robsonmvieira@gmail.com>
2020-05-12 13:54:29 -0300

    feat: update product controller methods

--- src/application/productController.ts
+++ src/application/productController.ts
@@ -1,14 +1,16 @@
 import {Request, Response} from 'express'
 import { container } from 'tsyringe'
-import Product from '@domain/contexts/product/entities/Product'
 import CreateProductService from '@domain/contexts/product/services/CreateProductService'
 import GetProductService from '@domain/contexts/product/services/GetProductsService'
 import GetProductByTitleService from '@domain/contexts/product/services/GetProductByTitleService'
 import GetProductByCompanyService from '@domain/contexts/product/services/GetProductByCompanyService'
+import UpdateProductService from '@domain/contexts/product/services/UpdateProductService'
+import GetProductByIdService from '@domain/contexts/product/services/GetProductByIdService'
+import RemoveProductService from '@domain/contexts/product/services/RemoveProductService'
 
 export default class ProductController {
 
-  public async create(req:Request,res: Response ):Promise<Product> {
+  public async create(req:Request,res: Response ):Promise<Response> {
     const service = container.resolve(CreateProductService)
     const result = await service.create(req.body)
     return res.status(201).json(result)
@@ -22,21 +24,35 @@ export default class ProductController {
 
 
   public async show(req:Request,res: Response ):Promise<Response> {
-    // const service = container.resolve(CreateCompanyService)
-    // const result = await service.create(req.body)
-    // return res.status(201).json(result)
+    const service = container.resolve(GetProductByIdService)
+    const {id} = req.params
+    const result = await service.exec(id)
+
+     if(result){
+       return res.status(201).json(result)
+     }
+     return res.status(400).json({error: 'product was not found'})
   }
 
   public async update(req:Request,res: Response ):Promise<Response> {
-    // const service = container.resolve(CreateCompanyService)
-    // const result = await service.create(req.body)
-    // return res.status(201).json(result)
+    const service = container.resolve(UpdateProductService)
+    const {id} = req.params
+    const result = await service.exec(id, req.body)
+
+    if(result){
+      return res.status(201).json(result)
+    }
+    return res.status(400).json({error: 'product was not found'})
   }
 
   public async remove(req:Request,res: Response ):Promise<Response> {
-    // const service = container.resolve(CreateCompanyService)
-    // const result = await service.create(req.body)
-    // return res.status(201).json(result)
+    const service = container.resolve(RemoveProductService)
+    const {id} = req.params
+    const result = await service.exec(id)
+    if(result){
+      return res.status(201).json(result)
+    }
+    return res.status(400).json({error: 'product was not found'})
   }
 
   public async getProductByTitle(req: Request, res: Response): Promise<Response>{
@@ -48,6 +64,7 @@ export default class ProductController {
     }
     return res.status(400).json({message: 'product was not found'})
   }
+
   public async getProductByCompany(req: Request, res: Response): Promise<Response>{
     const service = container.resolve(GetProductByCompanyService)
     const {id} = req.params
