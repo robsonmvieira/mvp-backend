4392d98c68635ae6217496ceb80f6eda03bf7dbd
Robson Maia <robsonmvieira@gmail.com>
2020-05-15 12:30:26 -0300

    feat: update DI Container

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -7,9 +7,15 @@ import IProductRepository from '@domain/contracts/IProductRepository'
 import ProductRepository from '@infra/repositories/ProductRepository'
 import IUserRepository from '@domain/contracts/IUserRepository'
 import UserRepository from '@infra/repositories/UserRepository'
+import IAvatarRepository from '@domain/contracts/IAvatarRepository'
+import AvatarRepository from '@infra/repositories/AvatarRepository'
+import IImageRepository from '@domain/contracts/IImageRepository'
+import ImageRepository from '@infra/repositories/ImageRepository'
 
 
 container.registerSingleton<ICompanyRepository>("CompanyRepository", CompanyRepository)
 container.registerSingleton<IAddressRepository>("AddressRepository", AddressRepository)
 container.registerSingleton<IUserRepository>("UserRepository", UserRepository)
 container.registerSingleton<IProductRepository>("ProductRepository", ProductRepository)
+container.registerSingleton<IAvatarRepository>("AvatarRepository", AvatarRepository)
+container.registerSingleton<IImageRepository>("ImageRepository", ImageRepository)
