fe59896088eacec544447a9a2ee739ab9278184b
Robson Maia <robsonmvieira@gmail.com>
2020-05-15 12:30:59 -0300

    feat: implement Image Repository

--- src/infra/repositories/ImageRepository.ts
+++ src/infra/repositories/ImageRepository.ts
@@ -1,4 +1,53 @@
-import { Repository } from "typeorm";
-import Image from "domain/contexts/users/entities/Image";
+import { Repository, getRepository } from "typeorm";
+import IImageRepository from '@domain/contracts/IImageRepository';
+import Image from '@domain/contexts/product/entities/Image';
 
-export default class ImageRepository extends Repository<Image> {}
+interface imagePost {
+  original_name: string;
+  name_saved: string ;
+  type: string;
+ }
+
+export default class ImageRepository implements IImageRepository {
+  private readonly orm: Repository<Image>
+
+  constructor() {
+    this.orm = getRepository(Image)
+  }
+
+  async create(image: imagePost): Promise<Image> {
+    const result = await this.orm.save(image)
+    return result
+  }
+
+  // async index(): Promise<Image[]> {
+  //   const users = await this.orm.find()
+  //   return users
+  // }
+
+  async show(id: string): Promise<Image | undefined> {
+   const imageExists = await this.orm.findOne(id)
+   if(imageExists) {
+     return imageExists
+   }
+   return undefined
+  }
+
+  async update(id: string, image: Image): Promise<boolean | undefined> {
+    const imageExists = await this.orm.findOne(id)
+    if(imageExists) {
+      await this.orm.update(id, image)
+      return true
+    }
+    return undefined
+  }
+
+  async remove(id: string): Promise<boolean> {
+    const imageExists = await this.orm.findOne(id)
+    if(imageExists) {
+      await this.orm.remove(imageExists)
+      return true
+    }
+    return false
+  }
+}
