7e6f774444a0c15e62ee06e667299d9b5ff4c115
Robson Maia <robsonmvieira@gmail.com>
2020-05-15 12:27:35 -0300

    feat: update User Routes

--- src/domain/contexts/users/routes/userRoutes.ts
+++ src/domain/contexts/users/routes/userRoutes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
-
+import multer from 'multer'
+import uploadConfig from '../../../../infra/utils/upload'
 import UserController from '@application/userController'
 
 const userController = new UserController()
 const routes = Router()
+const upload = multer(uploadConfig)
 
 routes.post('/', userController.create)
 routes.get('/', userController.index)
 routes.get('/:id', userController.show)
 routes.put('/:id', userController.update)
 routes.delete('/:id', userController.remove)
+routes.post('/:id/image', upload.single('image'), userController.updateImage)
 
 export default routes
