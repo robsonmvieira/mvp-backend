d3a380e7de88730e55581df9a2a15f01210aa701
Robson Maia <robsonmvieira@gmail.com>
2020-05-15 12:24:16 -0300

    feat: able upload to Product using multer config

--- src/domain/contexts/product/routes/productRoutes.ts
+++ src/domain/contexts/product/routes/productRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-
+import multer from 'multer'
+import uploadConfig from '../../../../infra/utils/upload'
 import ProductController from '@application/productController'
-
 const productController = new ProductController()
 const routes = Router()
+const upload = multer(uploadConfig)
 
 routes.post('/', productController.create)
 routes.get('/', productController.index)
@@ -12,5 +13,6 @@ routes.put('/:id', productController.update)
 routes.delete('/:id', productController.remove)
 routes.get('/byTitle/:title', productController.getProductByTitle)
 routes.get('/byCompany/:id', productController.getProductByCompany)
+routes.post('/:id/image', upload.single('image'),productController.updateImage)
 
 export default routes
