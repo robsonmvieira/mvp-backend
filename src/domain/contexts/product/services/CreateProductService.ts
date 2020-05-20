9ff4fb8147e0fe8388acdcf72ef39a7bba017b3d
Robson Maia <robsonmvieira@gmail.com>
2020-05-20 00:21:02 -0300

    feat: change signature create product service

--- src/domain/contexts/product/services/CreateProductService.ts
+++ src/domain/contexts/product/services/CreateProductService.ts
@@ -4,10 +4,18 @@ import ICreateProductService from "../contracts/ICreateProductService";
 import { inject, injectable } from "tsyringe";
 import ProductRepository from "@infra/repositories/ProductRepository";
 
+interface productDto  {
+  title: string
+  company_id: string
+  price: string
+  available?: boolean
+  image_id? : string
+}
+
 @injectable()
 export default class CreateProductService implements ICreateProductService {
   constructor(@inject('ProductRepository') private prodRepo: ProductRepository) {}
-  async create(product: Product): Promise<Product> {
+  async create(product: productDto): Promise<Product> {
     const result = await this.prodRepo.create(product)
     return result
   }
